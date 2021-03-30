356e2940a2485f6b4efeeac1391703e809420d21
天舟 <tianzhouchen@gmail.com>
2021-03-30 16:45:57 +0800

    Add inter font

--- projects/main/tailwind.config.js
+++ projects/main/tailwind.config.js
@@ -1,11 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
