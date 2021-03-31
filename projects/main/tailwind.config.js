61e6e063233b052bf68b31da58494c4a405e957c
天舟 <tianzhouchen@gmail.com>
2021-03-31 18:13:40 +0800

    purging unused tailwind css

--- projects/main/tailwind.config.js
+++ projects/main/tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [],
+  purge: ["index.html", "./pages/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
