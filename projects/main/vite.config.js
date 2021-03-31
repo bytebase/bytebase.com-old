b29b2847dd7112e243180f13e3fab46b8034cad0
天舟 <tianzhouchen@gmail.com>
2021-03-31 17:53:33 +0800

    Include file under pages

---
+++ projects/main/vite.config.js
@@ -0,0 +1,12 @@
+const { resolve } = require("path");
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        pages: resolve(__dirname, "pages/howwebuild.html"),
+      },
+    },
+  },
+};
