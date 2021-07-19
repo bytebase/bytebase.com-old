057f13d100541a53dcb793a7b3607c34503996ba
天舟 <tianzhouchen@gmail.com>
2021-07-19 19:40:44 +0800

    Add website description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,12 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Open source, zero-config, dependency-free, web-based database schema change and version control for Developers and DBAs",
+      },
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
