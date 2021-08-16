59bfe1d0a1c6e46230a6e7eed03b63b0e583050d
天舟 <tianzhouchen@gmail.com>
2021-08-16 16:59:08 +0800

    Improve footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,7 +4,7 @@
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="max-w-7xl mx-auto pt-12 px-4 sm:px-6 lg:pt-16 lg:px-8">
       <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-        <div class="space-y-6 xl:col-span-1">
+        <div class="xl:col-span-1 space-y-4">
           <img class="h-10" src="~/assets/logo.svg" alt="Bytebase" />
           <div class="flex space-x-6">
             <a
@@ -70,7 +70,7 @@
               &copy; 2021 Bytebase
             </p>
           </div>
-          <div class="text-base text-gray-500 hover:text-gray-900">
+          <div class="text-sm text-gray-500 hover:text-gray-900">
             <NuxtLink to="/privacy">
               Privacy Policy
             </NuxtLink>
@@ -172,6 +172,28 @@
               <ul class="mt-4 space-y-4">
                 <li>
                   <a
+                    href="https://github.com//bytebase/bytebase"
+                    target="__blank"
+                    class="flex flex-row space-x-2 text-base text-gray-500 hover:text-gray-900"
+                  >
+                    <span>GitHub</span>
+                    <svg
+                      class="h-6 w-6"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+
+                <li>
+                  <a
                     href="https://docs.bytebase.com/?ref=bytebase.com"
                     target="__blank"
                     class="text-base text-gray-500 hover:text-gray-900"
@@ -187,16 +209,6 @@
                     class="text-base text-gray-500 hover:text-gray-900"
                   >
                     Blog
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    href="https://github.com//bytebase/bytebase"
-                    target="__blank"
-                    class="text-base text-gray-500 hover:text-gray-900"
-                  >
-                    GitHub
                   </a>
                 </li>
               </ul>
@@ -221,9 +233,9 @@
                 <li>
                   <NuxtLink
                     to="/jobs"
-                    class="text-base text-gray-500 hover:text-gray-900"
+                    class="text-base text-gray-500 hover:text-gray-900 whitespace-nowrap"
                   >
-                    Careers - We're Hiring!
+                    Careers - We're Hiring! 🧲
                   </NuxtLink>
                 </li>
 
