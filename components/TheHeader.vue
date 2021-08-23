b8d2b1585a6ebfd00d24810ecc64ab5c7f290635
天舟 <tianzhouchen@gmail.com>
2021-08-24 02:51:30 +0800

    Header tweak in sm size

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,26 +1,32 @@
 <template>
   <header class="relative pt-6">
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
+      <div class="sm:hidden flex items-center justify-between w-full sm:w-auto">
+        <NuxtLink to="/">
+          <span class="sr-only">Bytebase</span>
+          <img class="h-6 sm:h-8 w-auto" src="~/assets/logo.svg" alt="" />
+        </NuxtLink>
+      </div>
       <nav
-        class="relative flex items-center justify-between sm:h-10 md:justify-center space-x-2"
+        class="relative flex items-center justify-between sm:h-10 sm:justify-center space-x-2"
         aria-label="Global"
       >
         <div
-          class="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0"
+          class="hidden sm:flex items-center flex-1 sm:absolute sm:inset-y-0 sm:left-0"
         >
-          <div class="flex items-center justify-between w-full md:w-auto">
+          <div class="flex items-center justify-between w-full sm:w-auto">
             <NuxtLink to="/">
               <span class="sr-only">Bytebase</span>
-              <img class="h-6 md:h-8 w-auto" src="~/assets/logo.svg" alt="" />
+              <img class="h-6 sm:h-8 w-auto" src="~/assets/logo.svg" alt="" />
             </NuxtLink>
           </div>
         </div>
-        <div class="flex items-center justify-between space-x-2 md:space-x-8">
+        <div class="flex items-center justify-between space-x-2 sm:space-x-8">
           <a
             href="https://demo.bytebase.com?ref=bytebase.com"
             target="_blank"
             @click="track('demo.header')"
-            class="text-blue-600 text-base md:text-xl font-semibold"
+            class="text-blue-600 text-base sm:text-xl font-semibold"
             style="box-shadow: rgb(255, 255, 255) 0px -0.166667em 0px 0px inset, rgb(186, 230, 253) 0px -0.333333em 0px 0px inset;"
           >
             Demo
@@ -28,21 +34,21 @@
           <NuxtLink
             to="/changelog"
             @click.native="track('changelog.header')"
-            class="text-gray-700 text-base md:text-xl font-semibold hover:text-gray-500 hover:underline whitespace-nowrap"
+            class="text-gray-700 text-base sm:text-xl font-semibold hover:text-gray-500 hover:underline whitespace-nowrap"
           >
             Changelog
           </NuxtLink>
           <NuxtLink
             to="/blog"
             @click.native="track('blog.header')"
-            class="text-gray-700 text-base md:text-xl font-semibold hover:text-gray-500 hover:underline whitespace-nowrap"
+            class="text-gray-700 text-base sm:text-xl font-semibold hover:text-gray-500 hover:underline whitespace-nowrap"
           >
             Blog
           </NuxtLink>
           <NuxtLink
             to="/jobs"
             @click.native="track('job.header')"
-            class="text-gray-700 text-base md:text-xl font-semibold hover:text-gray-500 hover:underline whitespace-nowrap"
+            class="text-gray-700 text-base sm:text-xl font-semibold hover:text-gray-500 hover:underline whitespace-nowrap"
           >
             Jobs🔥
           </NuxtLink>
@@ -50,7 +56,7 @@
             href="https://docs.bytebase.com?ref=bytebase.com"
             target="_blank"
             @click="track('doc.header')"
-            class="flex text-gray-700 text-base md:text-xl font-semibold hover:text-gray-500"
+            class="flex text-gray-700 text-base sm:text-xl font-semibold hover:text-gray-500"
           >
             Docs
             <svg
@@ -72,13 +78,13 @@
             href="https://github.com/bytebase/bytebase?ref=bytebase.com"
             target="_blank"
             @click="track('github.header')"
-            class="flex-shrink-0 md:hidden border-none rounded-full hover:opacity-80"
+            class="flex-shrink-0 sm:hidden border-none rounded-full hover:opacity-80"
           >
             <img class="h-8 w-auto" src="~/assets/github_logo.png" alt="" />
           </a>
         </div>
         <div
-          class="hidden md:flex absolute items-center justify-end right-0 space-x-4"
+          class="hidden sm:flex absolute items-center justify-end right-0 space-x-4"
         >
           <a
             href="https://github.com/bytebase/bytebase?ref=bytebase.com"
