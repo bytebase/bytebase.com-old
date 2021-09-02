8c26a99f07a9183efcccb5205fd0b6806f3a8bc7
天舟 <tianzhouchen@gmail.com>
2021-09-02 15:35:34 +0800

    Add subscribe newsletter on the webstie directly without jumping to the portal

--- components/SubscribeSection.vue
+++ components/SubscribeSection.vue
@@ -9,19 +9,37 @@
       </p>
     </div>
     <div class="mt-8 lg:mt-0 lg:ml-8">
-      <a
-        href="https://newsletter.bytebase.com/#/portal/signup"
-        @click="track()"
-        class="w-full flex items-center justify-center py-3 px-5 border border-transparent text-xl rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        Subscribe
-      </a>
+      <div v-if="subscribed" class="text-xl font-semibold text-indigo-600">
+        <span class="text-3xl mr-2">🙌</span> Now check {{ email }} to confirm
+        the subscription.
+      </div>
+      <form v-else class="sm:flex" @submit="subscribe">
+        <label for="email-address" class="sr-only">Email address</label>
+        <input
+          id="email-address"
+          name="email-address"
+          type="email"
+          autocomplete="email"
+          required
+          class="w-full px-5 py-3 border border-gray-300 shadow-sm placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs rounded-md"
+          placeholder="Enter your email"
+          v-model="email"
+        />
+        <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+          <button
+            type="submit"
+            class="w-full flex items-center justify-center py-3 px-5 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Subscribe
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@nuxtjs/composition-api";
+import { defineComponent, ref } from "@nuxtjs/composition-api";
 import Plausible from "plausible-tracker";
 
 const { trackEvent } = Plausible();
@@ -34,11 +52,28 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const track = () => {
+    const email = ref("");
+    const subscribed = ref(false);
+
+    const subscribe = (e: any) => {
       trackEvent(props.moduleName);
+      fetch("https://newsletter.bytebase.com/members/api/send-magic-link/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email.value,
+          name: "",
+          requestSrc: "bytebase.com",
+        }),
+      }).then(() => {
+        subscribed.value = true;
+      });
+      e.preventDefault();
     };
 
-    return { track };
+    return { email, subscribed, subscribe };
   },
 });
 </script>
