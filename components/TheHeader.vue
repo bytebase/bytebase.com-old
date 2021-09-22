5818d5d70faf8dce1eb353f8d7d956645fa27641
天舟 <tianzhouchen@gmail.com>
2021-09-22 12:35:08 +0800

    removec blog changelog for now, since they are down

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -37,18 +37,11 @@
           class="flex items-center justify-between text-sm sm:text-xl font-semibold space-x-2 sm:space-x-8"
         >
           <NuxtLink
-            to="/changelog"
-            @click.native="track('changelog.header')"
+            to="/jobs"
+            @click.native="track('job.header')"
             class="text-gray-700 hover:text-gray-500 hover:underline whitespace-nowrap"
           >
-            Changelog
-          </NuxtLink>
-          <NuxtLink
-            to="/blog"
-            @click.native="track('blog.header')"
-            class="text-gray-700 hover:text-gray-500 hover:underline whitespace-nowrap"
-          >
-            Blog
+            Jobs🔥
           </NuxtLink>
           <NuxtLink
             to="/database-glossary"
@@ -56,13 +49,6 @@
             class="text-gray-700 hover:text-gray-500 hover:underline whitespace-nowrap"
           >
             DB Glossary
-          </NuxtLink>
-          <NuxtLink
-            to="/jobs"
-            @click.native="track('job.header')"
-            class="text-gray-700 hover:text-gray-500 hover:underline whitespace-nowrap"
-          >
-            Jobs🔥
           </NuxtLink>
           <a
             href="https://docs.bytebase.com?ref=bytebase.com"
