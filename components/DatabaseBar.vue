f43b20a92bc3dfb91174e7f79bdf10ed7636e841
天舟 <tianzhouchen@gmail.com>
2021-11-07 00:02:11 +0800

    Fix database bar rendering on safari

--- components/DatabaseBar.vue
+++ components/DatabaseBar.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="flex flex-row h-12 space-x-4">
-    <div class="flex items-center mt-6 text-base tracking-tight text-gray-400">
+  <div class="flex flex-row space-x-4">
+    <div class="flex mt-6 text-base tracking-tight text-gray-400">
       Work with
     </div>
     <div class="flex flex-row space-x-6">
-      <img src="~/assets/logo/db-mysql.png" alt="mysql" />
-      <img src="~/assets/logo/db-postgres.png" alt="postgresql" />
-      <img src="~/assets/logo/db-tidb.png" alt="tidb" />
-      <img src="~/assets/logo/db-clickhouse.png" alt="clickhouse" />
-      <img src="~/assets/logo/gitlab-logo.svg" alt="gitlab" />
+      <img class="h-12 w-12" src="~/assets/logo/db-mysql.png" alt="mysql" />
+      <img
+        class="h-12 w-12"
+        src="~/assets/logo/db-postgres.png"
+        alt="postgresql"
+      />
+      <img class="h-12 w-12" src="~/assets/logo/db-tidb.png" alt="tidb" />
+      <img
+        class="h-12 w-12"
+        src="~/assets/logo/db-clickhouse.png"
+        alt="clickhouse"
+      />
+      <img class="h-12 w-12" src="~/assets/logo/gitlab-logo.svg" alt="gitlab" />
     </div>
   </div>
 </template>
