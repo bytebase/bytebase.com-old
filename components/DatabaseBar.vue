fe30cbfbec82cb07720706b555aef8e4308be7d0
天舟 <tianzhouchen@gmail.com>
2021-11-10 01:25:00 +0800

    Only specify height on databasebar icon so non-square icon can scale properly

--- components/DatabaseBar.vue
+++ components/DatabaseBar.vue
@@ -4,19 +4,15 @@
       With
     </div>
     <div class="flex flex-row space-x-6">
-      <img class="h-12 w-12" src="~/assets/logo/db-mysql.png" alt="mysql" />
+      <img class="h-12" src="~/assets/logo/db-mysql.png" alt="mysql" />
+      <img class="h-12" src="~/assets/logo/db-postgres.png" alt="postgresql" />
+      <img class="h-12" src="~/assets/logo/db-tidb.png" alt="tidb" />
       <img
-        class="h-12 w-12"
-        src="~/assets/logo/db-postgres.png"
-        alt="postgresql"
-      />
-      <img class="h-12 w-12" src="~/assets/logo/db-tidb.png" alt="tidb" />
-      <img
-        class="h-12 w-12"
+        class="h-12"
         src="~/assets/logo/db-clickhouse.png"
         alt="clickhouse"
       />
-      <img class="h-12 w-12" src="~/assets/logo/gitlab-logo.svg" alt="gitlab" />
+      <img class="h-12" src="~/assets/logo/gitlab-logo.svg" alt="gitlab" />
     </div>
   </div>
 </template>
