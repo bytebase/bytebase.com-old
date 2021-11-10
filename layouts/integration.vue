c3e925535125070d31409131b8d813308197e8ce
天舟 <tianzhouchen@gmail.com>
2021-11-11 01:15:01 +0800

    Minor integration layout tweak

--- layouts/integration.vue
+++ layouts/integration.vue
@@ -3,14 +3,14 @@
     <TheHeader />
     <main class="mt-8 sm:mt-16 max-w-7xl mx-auto px-4">
       <Nuxt />
-      <div class="flex mt-6 justify-center">
-        <DatabaseBar />
-      </div>
       <h2
         class="mt-16 text-center text-3xl sm:text-5xl font-extrabold text-gray-900"
       >
         Features
       </h2>
+      <div class="flex mt-12 justify-center">
+        <DatabaseBar />
+      </div>
       <FeatureSection />
     </main>
     <TheFooter />
