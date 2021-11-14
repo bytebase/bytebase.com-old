6813a460dcdae6d48a1702487a8eba2ea3aa1a5a
天舟 <tianzhouchen@gmail.com>
2021-11-14 16:26:09 +0800

    Add 404, 500

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex justify-center">
+    <img
+      v-if="error.statusCode == 404"
+      src="~/assets/illustration/404.webp"
+      alt=""
+    />
+    <img
+      v-else-if="error.statusCode == 500"
+      src="~/assets/illustration/500.webp"
+      alt=""
+    />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["error"],
+};
+</script>
