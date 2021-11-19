282eaa1d0442681058b6b4f0bbc68d7927a3dabe
天舟 <t@bytebase.com>
2021-11-19 12:25:02 +0800

    Add backers to the website

--- components/TeamSection.vue
+++ components/TeamSection.vue
@@ -11,7 +11,7 @@
             href="#team"
             class="text-3xl font-extrabold tracking-tight sm:text-4xl hover:underline"
           >
-            Meet our team
+            Meet our crew
           </a>
         </div>
         <ul
@@ -19,6 +19,52 @@
           class="mx-auto space-y-8 sm:grid grid-cols-2 sm:gap-8 sm:space-y-0 lg:grid-cols-4 lg:max-w-5xl"
         >
           <li v-for="person in people" :key="person.name">
+            <img
+              class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+              :src="require(`~/assets/people/${person.imageUrl}`)"
+              alt=""
+            />
+            <div class="space-y-2">
+              <div class="text-lg leading-6 font-medium space-y-1">
+                <template v-if="person.name == 'You'">
+                  <NuxtLink
+                    to="/jobs#jobs"
+                    class="
+                      text-2xl
+                      font-semibold
+                      text-indigo-600
+                      hover:text-indigo-500 hover:underline
+                      whitespace-nowrap
+                    "
+                  >
+                    {{ person.role }}🎢
+                  </NuxtLink>
+                </template>
+                <template v-else>
+                  <h3>{{ person.name }}</h3>
+                  <p class="text-indigo-600">{{ person.role }}</p>
+                </template>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div class="mt-16 space-y-12">
+        <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+          <a
+            id="backer"
+            href="#backer"
+            class="text-3xl font-extrabold tracking-tight sm:text-4xl hover:underline"
+          >
+            Backed by the best
+          </a>
+        </div>
+        <ul
+          role="list"
+          class="mx-auto space-y-8 sm:grid grid-cols-2 sm:gap-8 sm:space-y-0 lg:grid-cols-2 lg:max-w-5xl"
+        >
+          <li v-for="person in backer" :key="person.name">
             <img
               class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
               :src="require(`~/assets/people/${person.imageUrl}`)"
@@ -89,6 +135,19 @@ const people = [
   },
 ];
 
+const backer = [
+  {
+    name: "Matrix Partners China",
+    role: "",
+    imageUrl: "matrix.webp",
+  },
+  {
+    name: "Dongxu Huang",
+    role: "Co-Founder & CTO - PingCAP",
+    imageUrl: "dongxu.webp",
+  },
+];
+
 export default defineComponent({
   props: {},
   setup(props) {
@@ -99,7 +158,7 @@ export default defineComponent({
     //   people[i] = people[j];
     //   people[j] = temp;
     // }
-    return { people };
+    return { people, backer };
   },
 });
 </script>
