35f098463ee9622f5d691530b1f7c2f863227f41
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2021-11-23 09:39:33 +0800

    add Ji's avatar (#3)
    * add Ji's avatar
    
    * update Ji's role

--- components/TeamSection.vue
+++ components/TeamSection.vue
@@ -124,6 +124,11 @@ const people = [
     imageUrl: "zilong.webp",
   },
   {
+    name: "Ji",
+    role: "Engineer",
+    imageUrl: "ji.webp",
+  },
+  {
     name: "???",
     role: "Special Agent",
     imageUrl: "yaping.webp",
