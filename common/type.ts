8e1dbe2cc09f1ed5568cc797adb98ad5d52f7a49
天舟 <t@bytebase.com>
2021-12-08 00:45:21 +0800

    Update tag

--- common/type.ts
+++ common/type.ts
@@ -1,19 +1,11 @@
-export type PostTag =
-  | "Announcement"
-  | "Database Schema Design"
-  | "Getting Started"
-  | "Insights";
+export type PostTag = "Announcement" | "Education";
 
 export function postTagStyle(tag: PostTag): string {
   switch (tag) {
     case "Announcement":
       return "bg-indigo-100 text-indigo-800";
-    case "Database Schema Design":
-      return "bg-pink-100 text-pink-800";
-    case "Getting Started":
+    case "Education":
       return "bg-green-100 text-green-800";
-    case "Insights":
-      return "bg-yellow-100 text-yellow-800";
   }
 }
 
