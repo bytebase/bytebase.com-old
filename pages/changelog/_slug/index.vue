6f1dfc3dcefdae9a1f3840c05a976eeda223e765
ecmadao <wlec@outlook.com>
2022-03-04 14:55:01 +0800

    feat: add author avator and name in changelog

--- pages/changelog/_slug/index.vue
+++ pages/changelog/_slug/index.vue
@@ -12,7 +12,7 @@
     <span
       class="flex flex-row items-center justify-center block text-base text-gray-900 font-semibold tracking-wide uppercase"
     >
-      <div class="ml-2 flex space-x-1 text-gray-500">
+      <div class="ml-2 flex space-x-1 text-gray-500 items-center">
         <time :datetime="post.published_at">
           {{
             new Date(post.published_at).toLocaleString("default", {
@@ -26,12 +26,46 @@
           &middot;
         </span>
         <span> {{ post.reading_time }} min read </span>
+        <img
+          :src="post.authors[0].profile_image"
+          :alt="post.authors[0].profile_image"
+          class="w-6 h-6"
+        />
+        <span>{{ post.authors[0].name }}</span>
       </div></span
     >
     <div
       class="prose prose-indigo prose-xl md:prose-2xl mx-auto"
       v-html="post.html"
     ></div>
+
+    <div class="flex flex-col justify-center items-center py-10">
+      <div class="flex">
+        <div class="w-24 h-24 rounded-full bg-gray-100 flex items-center justify-center mr-5">
+          <img
+            :src="post.authors[0].profile_image"
+            :alt="post.authors[0].profile_image"
+            class="w-20 h-20"
+          />
+        </div>
+        <div class="flex flex-col justify-center">
+          <div class="text-2xl md:text-3xl font-medium mb-3">
+            {{ post.authors[0].name }}
+          </div>
+          <a
+            :href="post.authors[0].url"
+            target="_blank"
+            class="flex items-center hover:underline"
+          >
+            VIEW PROFILE
+            <!-- Heroicon name: outline/chevron-right -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
 
     <div class="border mt-8 max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-5xl">
       <ActionSection
