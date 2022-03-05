14626959e8877a6e1d254fbc3e683d9abcf1a454
ecmadao <wlec@outlook.com>
2022-03-05 11:06:03 +0800

    chore: change wording

--- pages/changelog/_slug/index.vue
+++ pages/changelog/_slug/index.vue
@@ -59,7 +59,7 @@
             target="_blank"
             class="flex items-center hover:underline"
           >
-            VIEW PROFILE
+            VIEW ARTICLES
             <!-- Heroicon name: outline/chevron-right -->
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
