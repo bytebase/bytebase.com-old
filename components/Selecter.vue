301f16b981658819ef0d8dd72ceef792ad296fa7
ecmadao <wlec@outlook.com>
2022-04-05 11:09:55 +0800

    chore: update selector ui

--- components/Selecter.vue
+++ components/Selecter.vue
@@ -1,6 +1,6 @@
 <template>
   <select
-    class="mt-1 block pl-3 pr-10 py-3 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+    class="select-reset select text-base bg-gray-100 border-gray-300 focus:outline-none sm:text-sm rounded-md"
     v-model="current"
   >
     <option v-for="option in options" :key="option.id" v-bind:value="option.id">
@@ -48,3 +48,36 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+
+.select-reset {
+  /* styling */
+  line-height: 1.5em;
+  padding: 0.5em 3.5em 0.5em 1em;
+
+  /* reset */
+  margin: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+}
+
+.select {
+  background-image:
+    linear-gradient(45deg, transparent 50%, gray 50%),
+    linear-gradient(135deg, gray 50%, transparent 50%),
+    linear-gradient(to right, #ccc, #ccc);
+  background-position:
+    calc(100% - 20px) calc(1em + 2px),
+    calc(100% - 15px) calc(1em + 2px),
+    calc(100% - 2.5em) 0.5em;
+  background-size:
+    5px 5px,
+    5px 5px,
+    1px 1.5em;
+  background-repeat: no-repeat;
+}
+</style>
