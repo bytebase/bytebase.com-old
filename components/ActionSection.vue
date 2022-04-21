4121d90ab757b37c763d0e82d7438109a84b3a97
天舟 <t@bytebase.com>
2022-04-21 18:41:05 +0800

    docs: use <NuxtLink> to point to our doc installation section

--- components/ActionSection.vue
+++ components/ActionSection.vue
@@ -3,9 +3,8 @@
     <div
       class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6 items-center whitespace-nowrap"
     >
-      <a
-        href="https://github.com/bytebase/bytebase#installation"
-        target="_blank"
+      <NuxtLink
+        to="/docs/install/install-with-docker"
         class="w-full flex items-center justify-center px-8 py-2 border border-transparent text-sm font-medium rounded-md border-indigo-700 text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-2xl md:px-8"
         @click="track('deploy')"
       >
@@ -27,7 +26,7 @@
             d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"
           ></path>
         </svg>
-      </a>
+      </NuxtLink>
       <a
         href="https://demo.bytebase.com?ref=bytebase.com"
         target="_blank"
