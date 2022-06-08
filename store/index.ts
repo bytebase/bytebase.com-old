1fcf8df3e2116550eb1fb377aec1759b567d01e0
boojack <imrealleonardo@gmail.com>
2022-06-08 08:01:08 +0800

    feat: add docsearch instead of pure algolia

--- store/index.ts
+++ store/index.ts
@@ -3,20 +3,10 @@ import { defineStore } from "pinia";
 export const useStore = defineStore("mainStore", {
   state: () => {
     return {
-      showSearchDialogFlag: false,
       hasRedirectLocale: false,
     };
   },
   actions: {
-    showSearchDialog() {
-      this.showSearchDialogFlag = true;
-    },
-    hideSearchDialog() {
-      this.showSearchDialogFlag = false;
-    },
-    toggleSearchDialog() {
-      this.showSearchDialogFlag = !this.showSearchDialogFlag;
-    },
     setHasRedirectLocale() {
       this.hasRedirectLocale = true;
     },
