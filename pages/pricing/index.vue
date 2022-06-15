e6d6c6f306c3d3b66afca6205674567631164c9c
ecmadao <wlec@outlook.com>
2022-06-15 16:07:40 +0800

    chore: update wording for free trial (#254)

--- pages/pricing/index.vue
+++ pages/pricing/index.vue
@@ -93,6 +93,12 @@
                 >
                   {{ plan.buttonText }}
                 </button>
+                <div
+                  v-if="plan.trialDays"
+                  class="font-bold text-sm my-2 text-center"
+                >
+                  {{ $t("pricing.free-trial") }}
+                </div>
               </div>
             </div>
             <h4 class="sr-only">Features</h4>
@@ -330,6 +336,12 @@
               >
                 {{ plan.buttonText }}
               </button>
+              <div
+                v-if="plan.trialDays"
+                class="font-bold text-sm my-2 text-center"
+              >
+                {{ $t("pricing.free-trial") }}
+              </div>
             </div>
           </div>
         </div>
@@ -525,11 +537,8 @@ export default defineComponent({
         return app.i18n.t("pricing.deploy-now") as string;
       if (plan.type === PlanType.ENTERPRISE)
         return app.i18n.t("pricing.contact-us") as string;
-      if (plan.trialDays && plan.trialPrice) {
-        return app.i18n.t("pricing.start-trial", {
-          price: plan.trialPrice,
-          days: plan.trialDays,
-        }) as string;
+      if (plan.trialDays) {
+        return app.i18n.t("pricing.start-trial") as string;
       }
       return app.i18n.t("pricing.subscribe-now") as string;
     };
