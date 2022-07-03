967140fa5485e2f2708082d3278d29bea67d151d
天舟 <t@bytebase.com>
2022-07-03 12:10:13 +0800

    docs: add a logo wall for past company

--- pages/index.vue
+++ pages/index.vue
@@ -80,7 +80,11 @@
       </div>
     </div>
 
-    <div class="mt-8 relative">
+    <div class="flex mt-8 justify-center">
+      <PastCompanyBar />
+    </div>
+
+    <div class="mt-12 relative">
       <div class="flex flex-col max-w-7xl mx-auto px-4 sm:px-6">
         <img
           class="order-2 relative lg:absolute shadow-lg mt-6 -ml-2 lg:ml-16 xl:ml-64 lg:z-10 lg:mt-96"
