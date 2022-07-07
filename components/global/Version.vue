d5fdc64b64a25b0075e4c2b84d80cfffc7b9c932
Megrax <geronimooo@foxmail.com>
2022-07-07 18:10:24 +0800

    chore: use plain text file to store version

--- components/global/Version.vue
+++ components/global/Version.vue
@@ -1,7 +1,9 @@
 <template>
-  <span>{{ VERSION.latest }}</span>
+  <span>{{ VERSION }}</span>
 </template>
 
 <script setup lang="ts">
-import VERSION from "../../VERSION.json";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { default as VERSION } from "!raw-loader!../../VERSION";
 </script>
