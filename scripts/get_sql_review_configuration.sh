7cf2da43706955c5616baf8c6d710f38e8d1b729
ecmadao <wlec@outlook.com>
2022-07-08 21:38:30 +0800

    chore: remove todo

--- scripts/get_sql_review_configuration.sh
+++ scripts/get_sql_review_configuration.sh
@@ -9,7 +9,7 @@ echo "schema review configuration file updated."
 
 LOCALIZATION_FOLDER="./locales/sql-review"
 mkdir -p $LOCALIZATION_FOLDER
-# TODO: change the url
+
 curl -o "$LOCALIZATION_FOLDER/en.json" "$URL/locales/sql-review/en-US.json"
 curl -o "$LOCALIZATION_FOLDER/zh.json" "$URL/locales/sql-review/zh-CN.json"
 
