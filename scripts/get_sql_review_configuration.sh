03c57a530196dce2dffef00ad91fd53f59d41e78
ecmadao <wlec@outlook.com>
2022-07-08 21:37:58 +0800

    chore: update script

--- scripts/get_sql_review_configuration.sh
+++ scripts/get_sql_review_configuration.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # TODO: use version to fetch the lastest file
 FILE="sqlReviewConfig.yaml"
-URL="https://raw.githubusercontent.com/bytebase/bytebase/main/frontend/src/types/$FILE"
+URL="https://raw.githubusercontent.com/bytebase/bytebase/main/frontend/src"
 
-curl -o "./common/$FILE" $URL
+curl -o "./common/$FILE" "$URL/types/$FILE"
 
 echo "schema review configuration file updated."
 
 LOCALIZATION_FOLDER="./locales/sql-review"
 mkdir -p $LOCALIZATION_FOLDER
 # TODO: change the url
-curl -o "$LOCALIZATION_FOLDER/en.json" "https://raw.githubusercontent.com/ecmadao/bytebase/chore/schema-review-api/frontend/src/locales/sql-review/en-US.json"
-curl -o "$LOCALIZATION_FOLDER/zh.json" "https://raw.githubusercontent.com/ecmadao/bytebase/chore/schema-review-api/frontend/src/locales/sql-review/zh-CN.json"
+curl -o "$LOCALIZATION_FOLDER/en.json" "$URL/locales/sql-review/en-US.json"
+curl -o "$LOCALIZATION_FOLDER/zh.json" "$URL/locales/sql-review/zh-CN.json"
 
 echo "schema review localization file updated."
