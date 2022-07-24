30099497e28502382e3beddb939edeb22dd41070
ecmadao <wlec@outlook.com>
2022-07-24 23:43:43 +0800

    chore: update the resource url for sql review (#373)
    * chore: update the resource url for sql review
    
    * chore: update script

--- scripts/fetch_sql_review_file.sh
+++ scripts/fetch_sql_review_file.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
-# TODO: use version to fetch the lastest file
 FILE="sqlReviewConfig.yaml"
-URL="https://raw.githubusercontent.com/bytebase/bytebase/main/frontend/src"
-
-curl -o "./common/$FILE" "$URL/types/$FILE"
-
-echo "Successfully updated SQL Review configuration file."
-
 LOCALIZATION_FOLDER="./locales/sql-review"
 mkdir -p $LOCALIZATION_FOLDER
 
-curl -o "$LOCALIZATION_FOLDER/en.json" "$URL/locales/sql-review/en-US.json"
-curl -o "$LOCALIZATION_FOLDER/zh.json" "$URL/locales/sql-review/zh-CN.json"
+VERSION=`cat ./VERSION`
+URL="https://raw.githubusercontent.com/bytebase/bytebase/$VERSION/frontend/src"
 
-echo "Successfully updated SQL Review localization file."
+input=(
+    "$URL/types/$FILE"
+    "$URL/locales/sql-review/en-US.json"
+    "$URL/locales/sql-review/zh-CN.json"
+)
+output=(
+    "./common/$FILE"
+    "$LOCALIZATION_FOLDER/en.json"
+    "$LOCALIZATION_FOLDER/zh.json"
+)
+
+for i in {0..2}
+do
+    echo "Start to fetch the SQL Review config file from ${input[$i]} to ${output[$i]}."
+    response=$(curl -w "%{http_code}" -o ${output[$i]} ${input[$i]})
+    http_code=$(tail -n1 <<< "$response")
+
+    if [ $http_code != 200 ]
+    then
+        echo "Failed to download the SQL Review config file from ${input[$i]}."
+        exit 1
+    fi
+done
+
+echo "Successfully updated the SQL Review config file."
