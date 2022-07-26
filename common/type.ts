6fa3a1470fa9a1e0d03a2fbd2d1877cc425c98c6
boojack <imrealleonardo@gmail.com>
2022-07-26 17:42:30 +0800

    chore: add default style

--- common/type.ts
+++ common/type.ts
@@ -8,6 +8,8 @@ export function postTagStyle(tag: PostTag): string {
       return "bg-green-100 text-green-800";
     case "Productivity":
       return "bg-blue-100 text-blue-800";
+    default:
+      return "bg-gray-100 text-gray-800";
   }
 }
 
