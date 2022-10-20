cf88f486baa3b00ca75e8912952f6e8d34e48f33
Tianzhou (天舟) <t@bytebase.com>
2022-10-20 15:19:52 +0800

    docs: add DevJoy link (#544)
    * docs: add DevJoy link
    
    * chore: mobile tweak

--- components/MainBanner.vue
+++ components/MainBanner.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="w-full relative bg-indigo-500">
+  <div
+    v-if="currentLocale === '简体中文'"
+    class="w-full relative border-b border-gray-900"
+    style="background-color: #ffd700"
+  >
+    <div class="max-w-7xl mx-auto py-2 px-2 sm:px-6 lg:px-8">
+      <div class="text-center sm:px-16">
+        <p class="font-medium text-slate-900">
+          <span class="hidden sm:inline"
+            >DevJoy 开发者游园会｜ 上海 WeWork 中国旗舰店 ｜ 2022.11.05 ~ 06
+          </span>
+          <span class="inline sm:hidden"
+            >DevJoy 开发者游园会 | 上海 2022.11.05 ~ 06
+          </span>
+          <span class="block sm:ml-2 sm:inline-block">
+            <a
+              href="https://app.ma.scrmtech.com/meetings/MeetingPc/Detail?pf_uid=14483_1648&sid=68471&source=2&pf_type=3&channel_id=4568&channel_name=Partner+WeChat+OA&tag_id=98362307a04a5e44&id=68471"
+              target="__blank"
+              class="text-slate-900 font-bold underline"
+            >
+              免费预约
+              <span aria-hidden="true">&rarr;</span>
+            </a>
+          </span>
+        </p>
+      </div>
+    </div>
+  </div>
+  <div v-else class="w-full relative bg-indigo-500">
     <div class="max-w-7xl mx-auto py-2 px-2 sm:px-6 lg:px-8">
       <div class="text-center sm:px-16">
         <p class="font-medium text-white">
@@ -22,11 +50,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@nuxtjs/composition-api";
+import { computed, defineComponent, useContext } from "@nuxtjs/composition-api";
 
 export default defineComponent({
   setup() {
-    // do nth
+    const { app } = useContext();
+
+    const currentLocale = computed(() => {
+      return (app.i18n.locales as any).find(
+        (i: any) => i.code === app.i18n.locale
+      ).name;
+    });
+
+    return { currentLocale };
   },
 });
 </script>
