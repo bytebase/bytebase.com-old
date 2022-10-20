7f894d0e161e063f834c9cc36b817756ef7b0864
tianzhou <t@bytebase.com>
2022-10-20 14:59:31 +0800

    docs: add DevJoy link

--- components/Footer.vue
+++ components/Footer.vue
@@ -244,6 +244,30 @@
                 </svg>
               </a>
             </li>
+
+            <li v-if="currentLocale === '简体中文'">
+              <a
+                href="https://devjoy.org"
+                target="__blank"
+                class="flex flex-row space-x-2 items-center text-base text-gray-500 hover:text-gray-900"
+              >
+                <span>DevJoy</span>
+                <svg
+                  class="w-5 h-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                  />
+                </svg>
+              </a>
+            </li>
           </ul>
         </div>
 
