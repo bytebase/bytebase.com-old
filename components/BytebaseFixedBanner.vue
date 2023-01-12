e013dc8e87780801baa1964cb75bd31fe818d082
boojack <sl@bytebase.com>
2023-01-12 19:01:38 +0800

    chore: add ad of Bytebase in TOC (#740)

---
+++ components/BytebaseFixedBanner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full hidden sm:block pl-4 pb-4 pt-2">
+    <a
+      class="relative bg-white w-full flex flex-col justify-center items-center rounded text-gray-500 hover:opacity-90 hover:text-blue-600 hover:underline"
+      href="https://bytebase.com/"
+      target="_blank"
+    >
+      <img
+        class="w-full h-auto shadow object-center"
+        src="~/assets/ads.png"
+        alt="bytebase"
+      />
+      <p class="mt-2 text-xs">
+        Database DevOps and CI/CD for MySQL, PG, ClickHouse, Snowflake, TiDB
+      </p>
+    </a>
+  </div>
+</template>
