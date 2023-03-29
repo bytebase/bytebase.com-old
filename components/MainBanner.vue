ea9c0644f6533b026f29e0299783dfce1ee36b89
Tianzhou Chen <t@bytebase.com>
2023-03-30 00:42:51 +0800

    chore: renaming sql chat post and add front banner

--- components/MainBanner.vue
+++ components/MainBanner.vue
@@ -3,10 +3,10 @@
     <div class="max-w-7xl mx-auto py-2 px-2 sm:px-6 lg:px-8">
       <div class="text-center sm:px-16">
         <p class="font-medium text-white">
-          <span class="inline">{{ $t("banner.cncf-landscape") }}</span>
+          <span class="inline">{{ $t("banner.sql-chat") }}</span>
           <span class="block sm:ml-2 sm:inline-block">
             <nuxt-link
-              :to="localePath('/blog/cncf-landscape')"
+              :to="localePath('/blog/sql-chat')"
               class="text-white font-bold underline"
             >
               {{ $t("banner.checkout-now") }}
